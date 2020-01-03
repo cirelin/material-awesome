1050383b4f6fa45ee3c1d06628fcf123c83bce04
Eric Lindsey <cirelin@gmail.com>
2020-01-03 21:01:35 +0300

    Update apps.lua

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -8,39 +8,42 @@ local rofi_command = 'env /usr/bin/rofi -dpi ' .. get_dpi() .. ' -width ' .. wit
 return {
   -- List of apps to start by default on some actions
   default = {
-    terminal = 'terminator',
+    terminal = 'lxterminal',
+    sysmonitor = 'stacer',
     rofi = rofi_command,
     lock = 'i3lock-fancy',
-    quake = 'terminator',
-    screenshot = '~/.config/awesome/configuration/utils/screenshot -m',
-    region_screenshot = '~/.config/awesome/configuration/utils/screenshot -r',
-    delayed_screenshot = 'sleep 10 ; ~/.config/awesome/configuration/utils/screenshot -r',
+    quake = '',
+    screenshot = '~/.config/awesome/configuration/utils/screenshot',
+    region_screenshot = 'flameshot gui',
+    delayed_screenshot = '~/.config/awesome/configuration/utils/screenshot -d 10',
     
     -- Editing these also edits the default program
     -- associated with each tag/workspace
-    browser = 'vivaldi-stable',
-    editor = 'gedit', -- gui text editor
-    social = 'discord',
-    game = rofi_command,
-    files = 'nautilus -w',
-    music = rofi_command
+    browser = 'opera',
+    editor = 'code',
+    text = 'mousepad', -- GUI text editor
+    social = 'mailspring',
+    game = 'steam-client',
+    files = 'nemo',
+    music = 'spotify',
+    other = rofi_command
   },
   -- List of apps to start once on start-up
   run_on_start_up = {
     'compton --config ' .. filesystem.get_configuration_dir() .. '/configuration/compton.conf',
-    'nm-applet --indicator', -- wifi
-    --'blueberry-tray', -- Bluetooth tray icon
-    --'xfce4-power-manager', -- Power manager
+    'nm-applet --indicator', -- Wifi
+    --'blueman-applet', -- Bluetooth
     'ibus-daemon --xim', -- Ibus daemon for keyboard
-    'scream -u -p 4011 -i virbr1', -- scream audio sink
-    'numlockx on', -- enable numlock
+    --'numlockx on', -- Enable numlock
     '/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
-    --KDE '/usr/lib/x86_64-linux-gnu/libexec/polkit-kde-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
-    -- MATE'/usr/lib/mate-polkit/polkit-mate-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
-     'flameshot',
-     'synology-drive -minimized',
-     'steam -silent',
-     'discord',
+    -- KDE  '/usr/lib/x86_64-linux-gnu/libexec/polkit-kde-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
+    -- MATE '/usr/lib/mate-polkit/polkit-mate-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
+    'flameshot', -- Area select screenshot tool
+    --'steam -silent', -- Start steam in background
+    --'discord --start-minimized', -- Start discord in background
+    'mailspring --background', -- Email client
+    'xfce4-power-manager', -- Power manager
+
     -- Add applications that need to be killed between reloads
     -- to avoid multipled instances, inside the awspawn script
     '~/.config/awesome/configuration/awspawn' -- Spawn "dirty" apps that can linger between sessions
